c737197c258003aa02c1e33e29d62e9415384a1f
DaniloNoboa <danilonoboamultimedia@gmail.com>
2026-02-02 16:49:07 -0500

    UPLOAD DEMO - PRODUCTOS BANCARIOS

---
+++ DEMO - PRODUCTOS BANCARIOS/story_content/user.js
@@ -0,0 +1,24 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+var nombre = player.GetVar("nombre");
+var capFirst = nombre.substring(0,1).toUpperCase() + nombre.substring(1,nombre.length);
+player.SetVar("nombre",capFirst);
+}
+
+};
